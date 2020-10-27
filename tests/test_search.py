8762275baeb60decedeee2dff965a8588fed2c61
zack-klein <klein.zachary.j@gmail.com>
2020-10-26 21:04:56 -0400

    test: Test that dataset tags show up on the search page

--- tests/test_search.py
+++ tests/test_search.py
@@ -6,10 +6,23 @@ from tests.utils import (
     get_admin,
     logout,
     build_dummy_data,
+    DUMMY_TAG,
 )
 
+from magellan.app import models
+from magellan.app.database import db
 
-def test_dummy_data_exists(client):  # noqa
+
+def test_not_logged_in_cant_see_search(client):
+    assert client.get("/search/").status_code != 200
+
+
+def test_admin_can_see_search(client):
+    admin_login(client)
+    assert client.get("/search/").status_code == 200
+
+
+def test_tags_suggested_for_search(client):
     admin_login(client)
     build_dummy_data(client)
-    logout(client)
+    assert bytes(DUMMY_TAG, "utf-8") in client.get("/search/").data
